476d451e7a4c9d376f76a885f7c7b223b796183a
Revina Victory <revina.victory@gmail.com>
2021-06-15 16:24:47 +0700

    trigger

---
+++ Clicker Neon Wave/Assets/Scripts/MoveRoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoad : MonoBehaviour
+{
+    private float offset = 0.2f;
+
+    void Update()
+    {
+        transform.position = new Vector3(0,0, transform.position.z + offset); 
+    }
+}
---
+++ Clicker Neon Wave/Assets/Scripts/RepeatBG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBG : MonoBehaviour
+{
+    
+    public float horizontalSize;
+    private Vector3 _offsetForvard;
+
+    void Update()
+    {
+        if (transform.position.z > -horizontalSize)
+            RepeatBackground();
+    }
+
+    private void RepeatBackground()
+    {
+        _offsetForvard = new Vector3(0, 0, horizontalSize);
+        transform.position = transform.position + _offsetForvard;
+    }
+}
+
